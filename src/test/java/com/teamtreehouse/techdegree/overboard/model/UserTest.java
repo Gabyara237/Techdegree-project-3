aa40b527a5bf06593ef8f2d1819b04cdf0c01427
Gabriela Araujo <110007206+Gabyara237@users.noreply.github.com>
2024-11-21 13:04:18 -0800

    Create increasingResponderReputation test method: test to verify that the responder's reputation is correctly increased to 10 points when someone upvotes the given answer.  Include .gitignore file

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -14,6 +14,9 @@ public class UserTest {
     private Question question2;
     private User userWhoAnswers2;
     private Answer answer2;
+    private User userNum3;
+    public int initialReputation;
+    public int actualReputation;
 
     @Before
     public void setUp() throws Exception {
@@ -22,20 +25,35 @@ public class UserTest {
 
         userNum1= board.createUser("Gabriel");
         userNum2 = board.createUser("Gabriela");
-
+        userNum3 = board.createUser("Andres");
         question = userNum1.askQuestion("What are the advantages of using composition Model in java unit test?");
 
+        answer= userNum2.answerQuestion(question,"When using the composition modal of code re-use, you are in more control of what public methods you expose, as well as the naming of the methods to your specific use case.");
 
     }
 
     @Test
     public void increasingQuestionerReputation()throws Exception {
 
-        int initialReputation = userNum1.getReputation();
+        initialReputation = userNum1.getReputation();
         userNum2.upVote(question);
-        int actualReputation = userNum1.getReputation();
+        actualReputation = userNum1.getReputation();
+       
         assertEquals(5,actualReputation-initialReputation);
 
     }
+
+    @Test
+    public void increasingResponderReputation() throws Exception{
+
+        initialReputation = userNum2.getReputation();
+        userNum1.upVote(answer);
+        actualReputation = userNum2.getReputation();
+
+        assertEquals(10, actualReputation-initialReputation);
+        
+    }
+
+
 }
 
